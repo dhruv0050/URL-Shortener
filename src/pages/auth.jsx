76f2f2db1b1edc90781965e8380ed30aef10681b
Dhruv Sharma <dhruvsh5467@gmail.com>
2025-03-18 21:55:59 +0530

    Auth Pages

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Login from '@/components/login'
+import Signup from '@/components/signup'
 
 const Auth = () => {
+  const[searchParamas] = useSearchParams()
   return (
-    <div>
-      Auth
+    <div className='mt-36 flex flex-col items-center gap-10'>
+      <h1 className='text-5xl font-extrabold'>
+      {searchParamas.get('createNew')?"Hold up! Let's login first":"Login or Signup"}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
+  <TabsList className='grid w-full grid-cols-2'>
+    <TabsTrigger value="login">Login</TabsTrigger>
+    <TabsTrigger value="signup">SignUp</TabsTrigger>
+  </TabsList>
+  <TabsContent value="login"><Login/></TabsContent>
+  <TabsContent value="signup"><Signup/></TabsContent>
+</Tabs>
+
     </div>
   )
 }
