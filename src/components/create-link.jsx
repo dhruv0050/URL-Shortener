ad67182a452a8fa478a203fb6f7d76b026d778cc
Dhruv Sharma <dhruvsh5467@gmail.com>
2025-04-03 20:08:08 +0530

    link fix

--- src/components/create-link.jsx
+++ src/components/create-link.jsx
@@ -118,7 +118,7 @@ export default function CreateLink() {
         />
         {errors.longUrl && <Error message={errors.longUrl} />}
         <div className="flex items-center gap-2">
-          <Card className="p-2">trim.in</Card> /
+          <Card className="p-2">url-shortener-brown-zeta.vercel.app</Card> /
           <Input
             id="customUrl"
             placeholder="Custom Link (optional)"
